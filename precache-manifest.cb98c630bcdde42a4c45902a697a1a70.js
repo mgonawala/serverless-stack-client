76d4d5c711e46532acc9e2edb3ddf23b3b97e22b
mgonawala <mohini.gonawala90@gmail.com>
2020-03-27 19:47:37 +0530

    Updates

---
+++ precache-manifest.cb98c630bcdde42a4c45902a697a1a70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "924be4f4de6e4fa16c070934ebbec143",
+    "url": "/serverless-stack-client/index.html"
+  },
+  {
+    "revision": "0f491d08af3072f2505a",
+    "url": "/serverless-stack-client/static/css/main.d29a5f18.chunk.css"
+  },
+  {
+    "revision": "5750c701cd6f24dbfed8",
+    "url": "/serverless-stack-client/static/js/2.4efb1768.chunk.js"
+  },
+  {
+    "revision": "ab92c4f55a310d19a4a3e0ce3bf2b7ba",
+    "url": "/serverless-stack-client/static/js/2.4efb1768.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f491d08af3072f2505a",
+    "url": "/serverless-stack-client/static/js/main.28935dcf.chunk.js"
+  },
+  {
+    "revision": "162ec6310f090d0351db",
+    "url": "/serverless-stack-client/static/js/runtime-main.58ab0cdb.js"
+  }
+]);
